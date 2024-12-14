e213b6e0e847f5cfbdd7e0e351552c10d5a67f43
Misael <misael.alisson14@gmail.com>
2024-12-14 17:13:23 -0300

    set up cache-manager and redis service in dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BookModule } from './modules/book/book.module';
@@ -6,9 +7,19 @@ import { ReviewsModule } from './modules/reviews/reviews.module';
 import { SeederModule } from './modules/seeder/seeder.module';
 import {MongoModule} from "./modules/mongo/mongo.module";
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+
+const redisStore = require('cache-manager-redis-store').redisStore;
 
 @Module({
   imports: [
+    CacheModule.register({
+      store: redisStore,
+      socket: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
     ConfigModule.forRoot({
       isGlobal: true, // Make the module global
       envFilePath: '.env',
@@ -18,7 +29,7 @@ import { ConfigModule } from '@nestjs/config';
     BookModule,
     UserModule,
     ReviewsModule,
-    SeederModule
+    SeederModule,
   ],
 })
 export class AppModule {}
